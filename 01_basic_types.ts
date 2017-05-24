14bc34f3e23377718a66946b7eeaeabf7816c743
Mark Craig <mcraig@emanatewireless.com>
2017-05-24 13:01:00 -0700

    Adds tuples

--- 01_basic_types.ts
+++ 01_basic_types.ts
@@ -17,3 +17,30 @@ whatever = "something else";
 whatever = false;
 
 console.log(whatever);
+/*
+** Basic Types
+*/
+
+// there are 3 basic types (boolean, number, string)
+var isOK: boolean = true;
+var answer: number = 42;
+var user: string = "Mark";
+
+console.log(isOK);
+console.log(answer);
+console.log(user);
+
+// use 'any' when type is not known
+var whatever: any = 42;
+whatever = "something else";
+whatever = false;
+
+console.log(whatever);
+
+/*
+** Tuples
+*/
+
+let tup: [number, string];
+tup = [42, "hi!"];
+console.log("tup0: " + tup[0] + ", tup1: " + tup[1]);
--- build/01_basic_types.js
+++ build/01_basic_types.js
@@ -13,4 +13,25 @@ var whatever = 42;
 whatever = "something else";
 whatever = false;
 console.log(whatever);
+/*
+** Basic Types
+*/
+// there are 3 basic types (boolean, number, string)
+var isOK = true;
+var answer = 42;
+var user = "Mark";
+console.log(isOK);
+console.log(answer);
+console.log(user);
+// use 'any' when type is not known
+var whatever = 42;
+whatever = "something else";
+whatever = false;
+console.log(whatever);
+/*
+** Tuples
+*/
+var tup;
+tup = [42, "hi!"];
+console.log("tup0: " + tup[0] + ", tup1: " + tup[1]);
 //# sourceMappingURL=01_basic_types.js.map
